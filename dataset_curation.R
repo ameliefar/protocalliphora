8477e486127e3243a810e7037579ca954125a6cf
Amélie Fargevieille <afargevieille@gmail.com>
2024-02-14 19:13:55 +0100

    :pencil2: fix issues related to cross_fostered broods

--- dataset_curation.R
+++ dataset_curation.R
@@ -15,31 +15,36 @@ chick <- openxlsx::read.xlsx(paste0(path, "/SIE POUS 1976-2023.xlsx"), detectDat
 
 # Selecting targeted variables from brood dataset
 sub_brood <- brood %>% 
-  dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble") %>%  # Select sites corresponding to "PIRIO" population and species Cyanistes caeruleus
-  dplyr::filter(!(is.na(proto))) %>% # Select rows with information on parasite load
-  dplyr::select(lieu, an, nic, proto, pulecl)
+  dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble") %>% # Select sites corresponding to "PIRIO" population and species Cyanistes caeruleus
+  dplyr::filter_at(vars(proto, pulecl), all_vars(!is.na(.))) %>%  # Select rows with information on parasite load and number of hatchlings
+  dplyr::filter(is.na(explique) | !(str_detect(explique, "p|P"))) %>% # Exclude cross-fostered broods
+  tidyr::pivot_longer(starts_with("pulbag"), names_to = "pulnumber", values_to = "bague") %>% 
+  dplyr::filter(!is.na(bague)) %>% 
+  dplyr::select(lieu, an, nic, bague, proto, pulecl, expou, experience, explique)
                    
 
 data_cond <- chick %>% 
   dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble" & age_plume > 13) %>% # Select sites, species and chick age (based on feather development)
   dplyr::select(lieu, an, nic, bague, tarsed, poids, age_plume) %>% 
-  dplyr::left_join(sub_brood, by = c("an", "lieu", "nic")) %>% # merge with brood dataset
-  dplyr::filter_at(vars(c(tarsed, poids)), any_vars(!is.na(.))) %>% # discard rows with no values for tarsus or mass
+  dplyr::left_join(sub_brood, by = "bague") %>% # merge with brood dataset
   dplyr::arrange(dplyr::desc(age_plume)) %>% # the following rows ensure there is no duplicate
   dplyr::group_by(bague) %>% 
   dplyr::slice(1) %>% 
   dplyr::ungroup() %>% 
-  dplyr::filter(an %in% c(2004:2021) & !is.na(proto)) %>% # filter targeted years and remove lack of information on parasite load
+  dplyr::filter(an.x %in% c(2004:2021) & !is.na(proto)) %>% # filter targeted years and remove lack of information on parasite load
+  dplyr::filter_at(vars(c(tarsed, poids)), any_vars(!is.na(.))) %>% # discard rows with no values for tarsus or mass
+  dplyr::filter_at(vars(c(tarsed, poids)), any_vars(!is.na(.))) %>% # discard rows with no values for tarsus or mass
   dplyr::mutate(ind_id = as.character(bague),
                 tarsus = as.numeric(tarsed),
                 mass = as.numeric(poids),
                 par_load = as.integer(proto),
                 nestling = as.numeric(pulecl),
-                year = as.character(an),
-                nestbox = paste(lieu, nic, sep = ""), # create nestbox identity
+                year = as.character(an.x),
+                nestbox_born = paste(lieu.y, nic.y, sep = ""), # create nestbox identity where the egg wais laid
+                nestbox_raised = paste(lieu.x, nic.x, sep = ""), # create nestbox identity where the nestling was raised (to detect cross-fostered individuals)
                 relative_par_load = round(par_load/nestling, digits = 2)) %>% # parasite load per nestling
-  dplyr::select(year, nestbox, ind_id, par_load, nestling, relative_par_load, tarsus, mass)
+  dplyr::select(year, nestbox_born, nestbox_raised, ind_id, par_load, nestling, relative_par_load, tarsus, mass) 
+
 
 write.csv(data_cond, paste0(data_path, "/nestling_condition.csv"))
 
-#Warning: a 1000 rows with NA for parasite load, I think it can be due to errors between brood dataset and chick dataset. I have to explore this
