5d1b04a47edb0af0bbfbb9c8e9684075a27128ad
Amélie Fargevieille <afargevieille@gmail.com>
2024-10-22 15:19:34 +0200

    :hammer: Update scripts with 2022 and 2023 added + fix missing variables/filters

--- dataset_curation.R
+++ dataset_curation.R
@@ -7,34 +7,35 @@ library(openxlsx)
 #'
 #'PRE-ANALYSIS : dataset to check the correlation between nestling physical condition and brood parasite load
 
-path <- "//Maison/SIE/Projets/Donnees_Mesange/1-BDD  LA PLUS RECENTE/1- Données (Démo, Morpho,Pous, Obs)/8-BDD validé"
-color_path <- "//Maison/SIE/Projets/Donnees_Mesange/1-BDD  LA PLUS RECENTE/3-BDD_Couleurs_Plumes/BDD_Col_A_JOUR"
+path <- "//Maison/SIE/Projets/Donnees_Mesange/1-BDD  LA PLUS RECENTE/1- Données (Démo, Morpho,Pous, Obs)/7-BDD en cours de saisie/"
+color_path <- "//Maison/SIE/Projets/Donnees_Mesange/1-BDD  LA PLUS RECENTE/3-BDD_Couleurs_Plumes/BDD_Col_A_JOUR/"
 data_path <- "C:/Users/FARGEVIEILLE/Documents/GitHub/protocalliphora/data"
-brood <- openxlsx::read.xlsx(paste0(path, "/SIE DEMO 1976-2023.xlsx"), detectDates = TRUE) #dataset on brood information
-chick <- openxlsx::read.xlsx(paste0(path, "/SIE POUS 1976-2023.xlsx"), detectDates = TRUE) #dataset related to chick information 
-adult <- openxlsx::read.xlsx(paste0(path, "/SIE MORPH 1976-2023.xlsx"), detectDates = TRUE) #dataset related to breeder information
-col <- openxlsx::read.xlsx(paste0(color_path, "/DB_Colour_2005-2021_19_03_2023.xlsx"), detectDates = TRUE) #datset related to color variables
+brood <- openxlsx::read.xlsx(paste0(path, "/SIE DEMO 1976-2024.xlsx"), detectDates = TRUE) #dataset on brood information
+chick <- openxlsx::read.xlsx(paste0(path, "/SIE POUS 1976-2024.xlsx"), detectDates = TRUE) #dataset related to chick information 
+adult <- openxlsx::read.xlsx(paste0(path, "/SIE MORPH 1976-2024.xlsx"), detectDates = TRUE) #dataset related to breeder information
+col <- readRDS(paste0(color_path, "/bdd_col_v2_ble_22102024.rds")) #datset related to color variables
 
 # Selecting targeted variables from brood dataset
 sub_brood <- brood %>% 
   dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble") %>% # Select sites corresponding to "PIRIO" population and species Cyanistes caeruleus
   dplyr::filter_at(vars(proto, pulecl), all_vars(!is.na(.))) %>%  # Select rows with information on parasite load and number of hatchlings
   dplyr::filter(is.na(explique) | !(str_detect(explique, "w|p|P"))) %>% # Exclude cross-fostered broods
-  tidyr::pivot_longer(starts_with("pulbag"), names_to = "pulnumber", values_to = "bague") %>% 
-  dplyr::filter(!is.na(bague)) %>% 
+  dplyr::filter(extnt != "T") %>% # Exclude nests which were experimentally treated for protocalliphora
+  tidyr::pivot_longer(starts_with("pulbag"), names_to = "pulnumber", values_to = "bague", values_drop_na = TRUE) %>% 
   dplyr::select(lieu, an, nic, bague, proto, pulecl, expou, experience, explique)
                    
 
 data_cond <- chick %>% 
   dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble" & age_plume > 13) %>% # Select sites, species and chick age (based on feather development)
-  dplyr::select(lieu, an, nic, bague, tarsed, poids, age_plume) %>% 
+  dplyr::select(lieu, an, nic, bague, tarsed, poids, age_plume, exp_p) %>% 
   dplyr::left_join(sub_brood, by = "bague") %>% # merge with brood dataset
   dplyr::arrange(dplyr::desc(age_plume)) %>% # the following rows ensure there is no duplicate
   dplyr::group_by(bague) %>% 
   dplyr::slice(1) %>% 
   dplyr::ungroup() %>% 
-  dplyr::filter(an.x %in% c(2004:2021) & !is.na(proto)) %>% # filter targeted years and remove lack of information on parasite load
-  dplyr::filter_at(vars(c(tarsed, poids)), any_vars(!is.na(.))) %>% # discard rows with no values for tarsus or mass
+  dplyr::filter(an.x %in% c(2004:2022) & !is.na(proto)) %>% # filter targeted years and remove lack of information on parasite load
+  dplyr::filter(if_any(c(tarsed, poids), ~(!is.na(.)))) %>% # discard rows with no values for tarsus or mass
+  dplyr::filter(is.na(exp_p)) %>% # exclude broods with experience on chicks (antibiotics/cross-fostering)
   dplyr::mutate(indID = as.character(bague),
                 tarsus = as.numeric(tarsed),
                 mass = as.numeric(poids),
@@ -52,27 +53,35 @@ write.csv(data_cond, paste0(data_path, "/nestling_condition.csv"), row.names = F
 #'PREDICTION 1
 # Select targeted data from color data set
 sub_col <- col %>% 
-  dplyr::filter(place %in% c("pir", "tua") & sp == "Cyanistes_caeruleus" & stringr::str_detect(age, "P1")) %>% # Select sites corresponding to "PIRIO" and blue tit species and first-year breeders who were born in the population
-  dplyr::select(ring, yr, place, nest, sex, per, BB, BUVC, YB, YUVC, YC, age, expe, exp_as_nestling) #183 rows (224 without removing birds with experiment)
+  dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble" & stringr::str_detect(age, "P1")) %>% # Select sites corresponding to "PIRIO" and blue tit species and first-year breeders who were born in the population
+  dplyr::select(bague, an, lieu, nic, sex, per, BB, BUVC, YB, YUVC, YC, age) #253 rows
+
+# Get broodID with an antibiotic experiment in 2022 (not appearing (yet) in brood table)
+antibio <- chick %>% 
+  filter(exp_p == "antibio" & lieu %in% c("pir", "tua")) %>% 
+  distinct(nic)
 
 # Select targeted data from brood data set
 sub_brood <- brood %>% 
-  dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble" & np == 1 & an %in% c(2004:2021) & !grepl("p|P|w", explique)) %>% # Select sites, species and targeted years
+  dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble" & np == 1 & an %in% c(2004:2022) & !grepl("p|P|w", explique)) %>% # Select sites, species and targeted years
+  dplyr::filter(extnt != "T") %>% # Exclude nests which were experimentally treated for protocalliphora
+  dplyr::filter(is.na(explique) | !(str_detect(explique, "w|p|P"))) %>%  # Exclude cross-fostered broods
+  dplyr::filter(!(nic %in% antibio$nic & an == 2022)) %>% # Remove broods with antibiotic experiment in 2022
   dplyr::select(lieu, nic, an, proto, pulecl, starts_with("pulbag")) %>% # Select columns of interest
-  tidyr::pivot_longer(cols = starts_with("pulbag"), names_to = NULL, values_to = "ring", values_drop_na = TRUE) %>% # nestling ID columns are transformed into individual rows
+  tidyr::pivot_longer(cols = starts_with("pulbag"), names_to = NULL, values_to = "bague", values_drop_na = TRUE) %>% # nestling ID columns are transformed into individual rows
   dplyr::filter(!is.na(proto)) # Remove rows without data on parasite load
-
+  
 # Merge data tables
-recruit <- dplyr::left_join(sub_col, sub_brood, by = c("ring")) %>% # Merge subsets
-  dplyr::filter(!is.na(lieu)) %>%  # Remove rows that lack brood information (no count of parasite load)
-  dplyr::mutate(indID = as.character(ring),
-                year = as.character(an),
+recruit <- dplyr::left_join(sub_col, sub_brood, by = c("bague")) %>% # Merge subsets
+  dplyr::filter(!is.na(lieu.y)) %>%  # Remove rows that lack brood information (no count of parasite load)
+  dplyr::mutate(indID = as.character(bague),
+                year = as.character(an.y),
                 sex = dplyr::case_when(sex == 1 ~ "M",
                                        sex == 2 ~ "F",
                                        TRUE ~ NA_character_),
                 period = dplyr::case_when(per == 2 ~ "C",
                                               per == 3 ~ "F"),
-                broodID = paste(lieu, nic, sep = ""),
+                broodID = paste(lieu.y, nic.y, sep = ""),
                 par_load = as.numeric(proto),
                 hatch_size = as.numeric(pulecl),
                 relative_par_load = round(par_load/hatch_size, digits = 2)) %>% 
@@ -91,21 +100,25 @@ write.csv(recruit, paste0(data_path, "/recruit_color.csv"), row.names = FALSE)
 #'
 # Select targeted data from color data set
 sub_col <- col %>% 
-  dplyr::filter(place %in% c("pir", "tua") & sp == "Cyanistes_caeruleus") %>% # Select sites corresponding to "PIRIO" and blue tit species
-  dplyr::select(ring, yr, place, nest, sex, age, per, BB, BUVC, YB, YUVC, YC) 
+  dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble") %>% # Select sites corresponding to "PIRIO" and blue tit species
+  dplyr::mutate(an = as.integer(an)) %>% 
+  dplyr::select(bague, lieu, an, lieu, ,nic, sex, age, per, BB, BUVC, YB, YUVC, YC) 
 
 # Select targeted data from brood data set
 sub_brood <- brood %>% 
-  dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble" & np == 1 & an %in% c(2004:2021)& !grepl("p|P|w", explique)) %>% # Select sites, species, targeted years and remove broods with experiments
+  dplyr::filter(lieu %in% c("pir", "tua") & espece == "ble" & np == 1 & an %in% c(2004:2023)& !grepl("p|P|w", explique)) %>% # Select sites, species, targeted years and remove broods with experiments
+  dplyr::filter(extnt != "T") %>% # Exclude nests which were experimentally treated for protocalliphora
+  dplyr::filter(!(nic %in% antibio$nic & an == 2022)) %>% # Remove broods with antibiotic experiment in 2022
   dplyr::select(lieu, nic, an, proto, pulecl, mbag, fbag) %>% # Select columns of interest
-  tidyr::pivot_longer(cols = c(mbag, fbag), names_to = NULL, values_to = "ring", values_drop_na = TRUE) %>% # breeder ID columns are transformed into individual rows
+  dplyr::mutate(pairID = paste(fbag, mbag, sep = "_")) %>%  # Create a variable pairID (female/male)
+  tidyr::pivot_longer(cols = c(mbag, fbag), names_to = NULL, values_to = "bague", values_drop_na = TRUE) %>% # breeder ID columns are transformed into individual rows
   dplyr::filter(!is.na(proto)) %>%  # Remove rows without data on parasite load 
-  dplyr::mutate(yr = an + 1) # Coin a variable corresponding to "year 2" to match data on color
+  dplyr::mutate(an = as.integer(an) + 1) # Coin a variable corresponding to "year 2" to match data on color
 
-adult <- dplyr::left_join(sub_col, sub_brood, by = c("ring", "yr")) %>% 
-  dplyr::filter(!is.na(lieu)) %>% # Remove rows with missing data
-  dplyr::mutate(broodID = paste(lieu, nic, sep = ""),
-                indID = as.character(ring),
+adult <- dplyr::left_join(sub_col, sub_brood, by = c("bague", "an")) %>% 
+  dplyr::filter(!is.na(lieu.y)) %>% # Remove rows with missing data
+  dplyr::mutate(broodID = paste(lieu.y, nic.y, sep = ""),
+                indID = as.character(bague),
                 year = as.character(an),
                 sex = dplyr::case_when(sex == 1 ~ "M",
                                        sex == 2 ~ "F",
@@ -116,9 +129,11 @@ adult <- dplyr::left_join(sub_col, sub_brood, by = c("ring", "yr")) %>%
                 hatch_size = as.numeric(pulecl),
                 relative_par_load = round(par_load/hatch_size, digits = 2),
                 min_age = dplyr::case_when(str_detect(age, "A") ~ as.integer(stringr::str_remove(age, "A")) + 1, #transform age into minimum age (A is for birds first tagged when at least 2 years old)
-                                            TRUE ~ as.integer(stringr::str_remove(age, "[PJI]")))) %>%  #P is for birds tagged as nestlings, J for birds tagged when one year old, I for birds tagged as breeders but unidentified age (so at least one year old)
+                                            TRUE ~ as.integer(stringr::str_remove(age, "[PJI]"))), #P is for birds tagged as nestlings, J for birds tagged when one year old, I for birds tagged as breeders but unidentified age (so at least one year old)
+                mateID = case_when(sex == "M" ~ str_remove_all(pairID, "_.*"),
+                                   sex == "F" ~ str_remove_all(pairID, ".*_"))) %>%  # Create a variable "mate ID" 
   dplyr::filter(!is.na(sex)) %>% # remove individuals with uncertainty on sex (possible when capture outside of reproduction)
-  dplyr::select(indID, sex, min_age, broodID, year, hatch_size, par_load, relative_par_load, period, BB, BUVC, YB, YUVC, YC) %>% 
+  dplyr::select(indID, sex, min_age, broodID, pairID, mateID, year, hatch_size, par_load, relative_par_load, period, BB, BUVC, YB, YUVC, YC) %>% 
   dplyr::arrange(year, indID, period) %>% 
   dplyr::group_by(indID, year) %>% 
   dplyr::slice(1) %>% # remove duplicates for birds (feather) sampled twice. The function "arrange" above allows to keep the first sample for the analyses
